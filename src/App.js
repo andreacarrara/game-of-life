1bef1385471fc35516d839218f3a9131334cc1a8
Andrea Carrara <andreacarrar@icloud.com>
2022-02-14 20:50:47 +0100

    Add grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
+import React, { useState } from 'react';
+
+const numRows = 20;
+const numCols = 20;
+
 function App() {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for (let i = 0; i < numRows; i++)
+      rows[i] = new Array(numCols).fill(0);
+
+    return rows;
+  });
+
   return (
-    <div>Game of Life</div>
+    <div style={{
+      backgroundColor: 'black',
+      border: '1px solid black',
+      display: 'inline-grid',
+      gridTemplateColumns: `repeat(${numCols}, 20px)`,
+      gridGap: 1
+    }}>
+      {grid.map((row, i) =>
+        row.map((cell, j) => (
+          <div
+            key={`${i}-${j}`}
+            style={{
+              width: 20,
+              height: 20,
+              backgroundColor: cell ? 'black' : 'white'
+            }}
+          />
+        ))
+      )}
+    </div>
   );
 }
 
